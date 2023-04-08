47ea93b51d45b0bb8dce02f98b2a7ef430409ce6
Anastasi <vashpopo97@mail.ru>
2023-04-08 16:34:22 +0300

    4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int Money = 300;  //Начальная сумма на счете
+        int RefundMoney = 4562; //Сумма пополнения
+        if (RefundMoney > 1000) {
+            int bonus = RefundMoney / 100;
+            System.out.println(Money+RefundMoney+bonus);
+        }
+        else {
+            if (RefundMoney<1000){
+                System.out.println(Money+RefundMoney);
+        }
+        }
     }
 }
